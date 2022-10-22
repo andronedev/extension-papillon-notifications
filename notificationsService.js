f5bb6aa761a33077fa5872b06446a7066b1569d0
andronedev <andronedev@gmail.com>
2022-10-22 13:35:38 +0200

    systeme de verification de conexion

--- notificationsService.js
+++ notificationsService.js
@@ -1,7 +1,33 @@
 const API_NOTIFICATIONS_URL = "http://localhost:3000";
 
 function isLogged() {
-    return localStorage.getItem("jwtNotificationsToken") !== null;
+    return new Promise(function (resolve, reject) {
+        let token = localStorage.getItem("jwtNotificationsToken")
+        if (token) {
+            fetch(API_NOTIFICATIONS_URL + "/login/?token=" + token, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then((response) => {
+                if (response.status == 200) {
+                    resolve(true)
+                } else {
+                    resolve(false)
+                }
+            }).then((data) => {
+                if (data.success) {
+                    return true
+                } else {
+                    resolve(false)
+                }
+            }).catch((error) => {
+                resolve(false)
+            })
+        } else {
+            resolve(false)
+        }
+    })
 }
 
 function openNotificationsSettings() {
@@ -14,17 +40,22 @@ function openNotificationsSettings() {
 
 }
 
-if (!isLogged()) {
-    Toastify({
-        text: "Vous n'avez pas encore configuré les notifications. Cliquez ici pour le faire.",
-        duration: 5000,
-        gravity: "top",
-        position: "right",
-        backgroundColor: "red",
-        stopOnFocus: true,
-        onClick: function () {
-            openNotificationsSettings();
-        }
-    }).showToast();
+isLogged().then((logged) => {
+    if (!logged) {
+        // remove token if it exists
+        localStorage.removeItem("jwtNotificationsToken")
 
-}
+        Toastify({
+            text: "Vous n'avez pas encore configuré les notifications. Cliquez ici pour le faire.",
+            duration: 5000,
+            gravity: "top",
+            position: "right",
+            backgroundColor: "red",
+            stopOnFocus: true,
+            onClick: function () {
+                openNotificationsSettings();
+            }
+        }).showToast();
+    }
+
+})
